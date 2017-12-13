111dbe82d3c175bb7f0462481d719e69306c5fde
Jimmy Romero <jerm.pcd@hotmail.com>
2017-12-13 13:18:45 -0500

    Repositorios conexiones

---
+++ AccesoDatosTSQL/DataAccessMysql.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace AccesoDatosTSQL
+{
+    public class DataAccessMysql: IDataAccess
+    {
+        private string _cadena = "server=172.30.5.2;user=root;database=empresa001;port=3306;password=conexion1234";
+        private MySqlConnection ObjConection = null;
+        private MySqlCommand ObjCommand = null;
+        private MySqlDataAdapter ObjAdap = null;
+        private DataTable ObjDT = null;
+
+        public DataAccessMysql()
+        {
+            if (ObjConection == null)
+            {
+                ObjConection = new MySqlConnection(_cadena);
+            }
+        }
+        public DataAccessMysql(string cadena_conexion)
+        {
+            _cadena = cadena_conexion;
+            if (ObjConection == null)
+            {
+                ObjConection = new MySqlConnection(_cadena);
+            }
+        }
+
+        public void OpenConection()
+        {
+                ObjConection.OpenAsync();
+        }
+
+        public void ClosedConection()
+        {
+            ObjConection.Close();
+        }
+
+        public void QuerySQL(String Consuta)
+        {
+            if (ObjCommand == null) {
+                ObjCommand = new MySqlCommand();
+            }
+            ObjCommand.Connection = ObjConection;
+            ObjCommand.CommandText = Consuta;
+        }
+
+        public int ExecQuery() {
+            ObjCommand.CommandType = CommandType.Text;
+            return ObjCommand.ExecuteNonQuery();
+        }
+
+        public DataTable ExecDataTable() {
+            if (ObjAdap == null) {
+                ObjAdap = new MySqlDataAdapter();
+            }
+            if (ObjDT == null) {
+                ObjDT = new DataTable();
+            }
+            ObjAdap.SelectCommand = ObjCommand;            
+            ObjAdap.Fill(ObjDT);
+            return ObjDT;
+        }
+
+        public void Parameters(string NameParameter, object Control) => ObjCommand.Parameters.AddWithValue(NameParameter, Control);
+
+        public void Dispose()
+        {
+            ObjConection.Close();
+        }
+    }
+}
